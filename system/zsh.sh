f2ec48e537347aef7a5a2960ce5eb5775318fc6c
Nelson Brandão <nelsonbrandao800@gmail.com>
2017-02-03 18:34:40 +0000

    Added git branches alias and some minor zsh configs

--- system/zsh.sh
+++ system/zsh.sh
@@ -1,8 +1,14 @@
-# Settings
-setopt autocd nomatch
+## Settings
+# awesome cd movements from zshkit
+setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars nomatch
+DIRSTACKSIZE=5
+
+# Enable extended globbing
+setopt extendedglob
+
 unsetopt beep notify
 
-# Keybing
+## Keybing
 typeset -A key
 
 key[Up]=${terminfo[kcuu1]}
@@ -10,3 +16,9 @@ key[Down]=${terminfo[kcud1]}
 
 [[ -n "${key[Up]}"       ]]  && bindkey  "${key[Up]}"       history-beginning-search-backward
 [[ -n "${key[Down]}"     ]]  && bindkey  "${key[Down]}"     history-beginning-search-forward
+
+## Color
+autoload -U colors
+colors
+
+export CLICOLOR=1
